7e277100d83470786d762c5c3121a67fcc8f9bda
Rupom Ahsan <rupom@live.com>
2022-11-18 21:14:34 +0800

    Updated Storage Options

--- scripts/tinyservice.sh
+++ scripts/tinyservice.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 dotnet run --project src/TinyService
+
+# To install docker Image
+docker build -t tinyservice .
+
+# kubectl alias command
+
+Set-Alias -Name k -Value kubectl 
+
+# install ngnix
+docker pull nginx
+
+# Install alpine
+docker pull alpine
